47cc7cb5ffd1e589af618af1a600f384e9b05686
Kawashima_Y <146790933+Yuki-K-03@users.noreply.github.com>
2023-10-30 23:16:30 +0900

    プロジェクト ファイルを追加します。

---
+++ 2023_10_24_Task_1/2023_10_24_Task_1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    int count = 0;
+    int Divisor = 0;
+
+    cin >> num;
+
+    for (int j = 1; j <= num; j+=2) {
+        for (int i = 1; i <= j; i++) {
+            if (j % i == 0) {
+                count++;
+            }
+        }
+        if (count == 8) {
+            printf("%d ", j);
+            Divisor++;
+        }
+        count = 0;
+    }
+    printf("\n合計%d個\n", Divisor);
+}
